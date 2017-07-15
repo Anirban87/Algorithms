35d2ebfc7b7c0fd10c576d35b21474125aee6eb6
Anirban87 <onie87@icloud.com>
2017-07-16 04:06:24 +0530

    bubble sort

---
+++ bubblesort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main()
+{
+  int n =7; //size of the array
+  int A[n] = {4,7,1,3,9,2,5};
+  int temp;
+
+  std::cout<<"Initial array is : ";
+
+  for(int i=0 ; i<n ; i++)
+  {
+    std::cout<<A[i];
+  }
+
+  //bubble sort logic
+  for(int j=0 ; j<n ; j++)
+  {
+      for(int k=0 ; k<n-1 ; k++)      //liner search and swap the elements
+      {
+          if(A[k] > A[k+1])
+          {
+              temp = A[k];
+              A[k] = A[k+1];
+              A[k+1] = temp;
+          }
+      }
+  }
+
+ std::cout<<"\nArray after bubble sort : ";
+
+  for(int l=0 ; l<n ; l++)
+  {
+    std::cout<<A[l];
+  }
+
+  std::cout<<"\n";
+
+  return 0;
+}
